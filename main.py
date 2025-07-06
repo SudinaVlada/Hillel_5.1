cdf9545188989884d4d5ab6091cc339c05a8f87b
Владислава <sudina.vlada@gmail.com>
2025-07-06 16:24:00 +0300

    commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import string
+import keyword
+
+text = input("Введіть вашу змінну: ")
+list1 = []
+for char in string.punctuation:
+    if char != "_":
+        list1.append(char)
+for char in text:
+    if char in string.ascii_uppercase:
+        print(False)
+    break
+if text[0].isdigit():
+        print(False)
+        exit()
+for i in list1:
+        if i in text:
+            print(False)
+            break
+if text.count("_") > 1:
+        print(False)
+        exit()
+if text in keyword.kwlist:
+        print(False)
+        exit()
+print(True)
